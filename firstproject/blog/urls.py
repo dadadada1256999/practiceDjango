b08b6359a690585ff27e9e946fa4a7f7cd3ba854
dadadada1256999 <base.dai258@gmail.com>
2024-04-28 10:58:09 +0900

    ブログサイトの追加

--- firstproject/blog/urls.py
+++ firstproject/blog/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from . import views
 
 # URLパターンを逆引きできるように名前を付ける
+# {% url 'blog:contact' %}でindex.htmlから呼び出したとき、app_name:urlpatternsでマッチングする
 app_name = 'blog'
 
 # URLパターンを登録するための変数
@@ -12,5 +13,10 @@ app_name = 'blog'
 # viewsモジュールのIndexViewクラスをインスタンス化する
 # そのインスタンスのas_view()メソッドを呼び出す
 urlpatterns = [
+    # クラスのパターン
     path('', views.IndexView.as_view(), name='index'),
+    # 関数のパターン
+    path('about', views.about, name='about'),
+    path('post', views.post, name='post'),
+    path('contact', views.contact, name='contact')
 ]
--- firstproject/blog/views.py
+++ firstproject/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.views.generic.base import TemplateView
 
+# クラスのパターン
 class IndexView(TemplateView):
     '''トップページのビュー
     テンプレートのレンダリングに特化したTemplateViewを継承
@@ -10,3 +11,31 @@ class IndexView(TemplateView):
         template_name (str): レンダリングするテンプレートのパス
     '''
     template_name = 'index.html'
+
+# 関数のパターン
+def about(request):
+    '''アバウトのビュー
+    テンプレートのレンダリングに特化したTemplateViewを継承
+
+    Attributes:
+        template_name (str): レンダリングするテンプレートのパス
+    '''
+    return render(request, 'about.html')
+
+def post(request):
+    '''postのビュー
+    テンプレートのレンダリングに特化したTemplateViewを継承
+
+    Attributes:
+        template_name (str): レンダリングするテンプレートのパス
+    '''
+    return render(request, 'post.html')
+
+def contact(request):
+    '''アバウトのビュー
+    テンプレートのレンダリングに特化したTemplateViewを継承
+
+    Attributes:
+        template_name (str): レンダリングするテンプレートのパス
+    '''
+    return render(request, 'contact.html')
